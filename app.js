1196cfc5146a23126d4a115c6853fd17c52a3cf2
Josh-Decime <josh_decime@yahoo.com>
2023-11-29 14:45:25 -0700

    Have all img, just getting started in JS

---
+++ app.js
@@ -0,0 +1,67 @@
+console.log('the sun has set')
+
+const locations = [
+    'Estate.png', 'Church.png', 'Hospital.png', 'Crypt.png', 
+]
+
+const people = [{
+        name: 'Cathrine',
+        picture: 'Cathrine.png',
+        location: 'estateID'
+    },
+    {
+        name: 'Hector',
+        picture: 'Hector.png',
+        location: 'estateID'
+    },
+    {
+        name: 'Hyde',
+        picture: 'Hyde.png',
+        location: 'estateID'
+    },
+    {
+        name: 'Jenny',
+        picture: 'Jenny.png',
+        location: 'churchID'
+    },
+    {
+        name: 'Jim',
+        picture: 'Jim.png',
+        location: 'churchID',
+    },
+    {
+        name: 'Julie',
+        picture: 'Julie.png',
+        location: 'churchID',
+    },
+    {
+        name: 'Keith',
+        picture: 'Keith.png',
+        location: 'hospitalID',
+    },
+    {
+        name: 'Margret',
+        picture: 'Margret.png',
+        location: 'hospitalID',
+    },
+    {
+        name: 'Suzy',
+        picture: 'Suzy.png',
+        location: 'hospitalID',
+    },
+    {
+        name: 'Tommy',
+        picture: 'Tommy.png',
+        location: 'cryptID',
+    },
+    {
+        name: 'VanGogh',
+        picture: 'VanGogh.png',
+        location: 'cryptID',
+    },
+    {
+        name: 'Victoria',
+        picture: 'Victoria.png',
+        location: 'cryptID',
+    }
+]
